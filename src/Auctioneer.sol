768d5e0f5b7048497b61bf3b7f54d1b933fc7f2b
Arr00 <13561405+arr00@users.noreply.github.com>
2023-09-23 17:46:45 -0400

    working using name wrapper

--- src/Auctioneer.sol
+++ src/Auctioneer.sol
@@ -1,11 +1,14 @@
 /// SPDX-LICENSE: MIT
 pragma solidity ^0.8.13;
 
-contract Auctioneer {
+import {ENSManager, BaseRegistrarImplementation, ENSRegistryWithFallback, NameWrapper} from "./ENSManager.sol";
+
+contract Auctioneer is ENSManager {
     error NotEnoughEth();
     error AuctionAlreadyExists();
     error AuctionNotLive();
     error AuctionNotExecutable();
+    error NodeExists();
 
     mapping(bytes32 => Auction) public auctions;
     uint72 public constant MIN_BID = 0.001 ether;
@@ -19,6 +22,12 @@ contract Auctioneer {
         uint40 startTime;
     }
 
+    constructor(
+        BaseRegistrarImplementation ens,
+        ENSRegistryWithFallback ensRegistry,
+        NameWrapper nameWrapper
+    ) ENSManager(ens, ensRegistry, nameWrapper) {}
+
     function lick(bytes32 node, bytes32 label) external payable {
         if (msg.value != MIN_BID) {
             revert NotEnoughEth();
@@ -26,6 +35,10 @@ contract Auctioneer {
 
         if (auctions[keccak256(abi.encodePacked(node, label))].startTime == 0) {
             revert AuctionAlreadyExists();
+        }
+
+        if (_ensNodeExists(keccak256(abi.encodePacked(node, label)))) {
+            revert NodeExists();
         }
 
         auctions[keccak256(abi.encodePacked(node, label))] = Auction({
@@ -56,9 +69,10 @@ contract Auctioneer {
         auction.leadingBidder = payable(msg.sender);
     }
 
-    function finalize(bytes32 node, bytes32 label) external payable {
+    function finalize(bytes32 node, string memory label) external payable {
+        bytes32 labelHash = keccak256(abi.encodePacked(node, label));
         Auction memory auction = auctions[
-            keccak256(abi.encodePacked(node, label))
+            keccak256(abi.encodePacked(node, labelHash))
         ];
         if (
             auction.startTime == 0 ||
@@ -67,7 +81,10 @@ contract Auctioneer {
             revert AuctionNotExecutable();
         }
         // Winner gets second price
-        auction.leadingBidder.transfer(auction.firstPrice - auction.secondPrice);
-        delete auctions[keccak256(abi.encodePacked(node, label))];
+        auction.leadingBidder.transfer(
+            auction.firstPrice - auction.secondPrice
+        );
+        _setSubnodeOwner(node, label, auction.leadingBidder);
+        delete auctions[keccak256(abi.encodePacked(node, labelHash))];
     }
 }
--- src/ENSManager.sol
+++ src/ENSManager.sol
@@ -2,7 +2,104 @@
 pragma solidity ^0.8.13;
 
 import {BaseRegistrarImplementation} from "ens-contracts/ethregistrar/BaseRegistrarImplementation.sol";
+import {ENSRegistryWithFallback} from "ens-contracts/registry/ENSRegistryWithFallback.sol";
+import {NameWrapper} from "ens-contracts/wrapper/NameWrapper.sol";
+import {IERC1155Receiver} from "@openzeppelin/contracts/interfaces/IERC1155Receiver.sol";
 
-abstract contract ENSManager {
+abstract contract ENSManager is IERC1155Receiver {
+    error NotOwner();
+    error NodeNotOwned();
+    error InvalidTokenAmount();
+    error InvalidTokenSender();
 
+    BaseRegistrarImplementation public immutable ENS;
+    bytes32 public immutable ENS_BASE_NODE;
+    ENSRegistryWithFallback public immutable ENS_REGISTRY;
+    NameWrapper public immutable NAME_WRAPPER;
+
+    mapping(bytes32 => address) public nodeOwners;
+
+    constructor(
+        BaseRegistrarImplementation ens,
+        ENSRegistryWithFallback ensRegistry,
+        NameWrapper nameWrapper
+    ) {
+        ENS = ens;
+        ENS_BASE_NODE = ENS.baseNode();
+        ENS_REGISTRY = ensRegistry;
+        NAME_WRAPPER = nameWrapper;
+    }
+
+    function deposit(uint256 tokenId) external {
+        NAME_WRAPPER.safeTransferFrom(
+            msg.sender,
+            address(this),
+            tokenId,
+            1,
+            ""
+        );
+        nodeOwners[bytes32(tokenId)] = msg.sender;
+    }
+
+    function withdraw(uint256 tokenId) external {
+        if (nodeOwners[bytes32(tokenId)] != msg.sender) {
+            revert NotOwner();
+        }
+        NAME_WRAPPER.safeTransferFrom(
+            address(this),
+            msg.sender,
+            tokenId,
+            1,
+            ""
+        );
+        delete nodeOwners[bytes32(tokenId)];
+    }
+
+    function _setSubnodeOwner(
+        bytes32 node,
+        string memory label,
+        address owner
+    ) internal returns (bytes32) {
+        if (nodeOwners[node] == address(0)) {
+            revert NodeNotOwned();
+        }
+        return NAME_WRAPPER.setSubnodeOwner(node, label, owner, 0, 0);
+    }
+
+    function _ensNodeExists(bytes32 node) internal view returns (bool) {
+        return ENS_REGISTRY.owner(node) != address(0);
+    }
+
+    function onERC1155Received(
+        address,
+        address from,
+        uint256 tokenId,
+        uint256 value,
+        bytes calldata
+    ) external returns (bytes4) {
+        if (msg.sender != address(NAME_WRAPPER)) {
+            revert InvalidTokenSender();
+        }
+        if (value != 1) {
+            revert InvalidTokenAmount();
+        }
+        nodeOwners[bytes32(tokenId)] = from;
+        return IERC1155Receiver.onERC1155Received.selector;
+    }
+
+    function onERC1155BatchReceived(
+        address,
+        address,
+        uint256[] calldata,
+        uint256[] calldata,
+        bytes calldata
+    ) external returns (bytes4) {
+        revert();
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    ) external view returns (bool) {
+        return interfaceId == type(IERC1155Receiver).interfaceId;
+    }
 }
